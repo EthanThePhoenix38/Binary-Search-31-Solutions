8b82eb1f6b20dffce19af26c0a3fa086c3363c09
PhoenixProject <103653068+EthanThePhoenix38@users.noreply.github.com>
2026-01-08 16:14:25 +0100

    Fix imports - remove deleted alternative functions
    Removed unused functions from __all__ export.

--- src/__init__.py
+++ src/__init__.py
@@ -13,12 +13,10 @@ Date: 2026-01-08
 __version__ = "1.0.0"
 __author__ = "EthanThePhoenix38"
 
-from .problem1_optimize_routes import optimize_air_routes, optimize_air_routes_two_pointer
-from .problem2_h_index_ii import h_index, h_index_linear
+from .problem1_optimize_routes import optimize_air_routes
+from .problem2_h_index_ii import h_index
 
 __all__ = [
     "optimize_air_routes",
-    "optimize_air_routes_two_pointer",
     "h_index",
-    "h_index_linear",
 ]
